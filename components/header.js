b292ac562a206b8d2d64dbe63d28264e3d0d1095
DarenGit28 <daren.shamoun28@gmail.com>
2023-06-28 15:43:59 -0700

    added a basic header and footer

---
+++ components/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-xl">Dashbyte AI</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+  
