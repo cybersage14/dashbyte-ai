1649546cfa1f6be702adfde83ccd5aa6de3b682a
DarenGit28 <daren.shamoun28@gmail.com>
2023-06-28 19:32:19 -0700

    styled the header and footer

--- components/footer.js
+++ components/footer.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 px-4 text-center">
+    <footer className="bg-gradient-to-r from-green-400 to-blue-500 text-white py-8 px-4 text-center">
       <p className="mb-4">
         &copy; {new Date().getFullYear()} Dashbyte AI. All rights reserved.
       </p>
       <div className="flex justify-center space-x-4">
         <a href="https://www.facebook.com/dashbyte/" target="_blank" rel="noreferrer">
-          <img src="/assets/facebook.svg" alt="Facebook" className="h-6 w-6" />
+          <img src="/images/facebook.svg" alt="Facebook" className="h-6 w-6" />
         </a>
         <a href="https://twitter.com/dash_byte" target="_blank" rel="noreferrer">
-          <img src="/assets/twitter.svg" alt="Twitter" className="h-6 w-6" />
+          <img src="/images/twitter.svg" alt="Twitter" className="h-6 w-6" />
         </a>
         <a href="https://www.instagram.com/dashbyte/" target="_blank" rel="noreferrer">
-          <img src="/assets/instagram.svg" alt="Instagram" className="h-6 w-6" />
+          <img src="/images/instagram.svg" alt="Instagram" className="h-6 w-6" />
         </a>
       </div>
     </footer>
--- components/header.js
+++ components/header.js
@@ -3,28 +3,27 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-gradient-to-r from-green-400 to-blue-500 p-4 flex justify-between items-center">
       <Link href="/">
-        <a>
-          <img src="/logo.png" alt="Logo" className="h-8" />
-        </a>
+        <div className="bg-black p-1 rounded">
+          <img src="/images/4k-LogoTransparent.png" alt="Logo" className="h-8" />
+        </div>
       </Link>
       <ul className="flex space-x-4">
-        <li><Link href="/"><a className="text-white">Home</a></Link></li>
-        <li><Link href="/pc-builder"><a className="text-white">PC Builder</a></Link></li>
-        <li><Link href="/services"><a className="text-white">Services</a></Link></li>
-        <li><Link href="/about"><a className="text-white">About</a></Link></li>
-        <li><Link href="/contact"><a className="text-white">Contact</a></Link></li>
+        <li><Link href="/pc-builder" className="text-white">PC Builder</Link></li>
+        <li><Link href="/services" className="text-white">Services</Link></li>
+        <li><Link href="/about" className="text-white">About</Link></li>
+        <li><Link href="/contact" className="text-white">Contact</Link></li>
       </ul>
       <div className="flex space-x-4">
         <a href="https://www.facebook.com/dashbyte/" target="_blank" rel="noreferrer">
-          <img src="/facebook.svg" alt="Facebook" className="h-6" />
+          <img src="/images/facebook.svg" alt="Facebook" className="h-6" />
         </a>
         <a href="https://twitter.com/dash_byte" target="_blank" rel="noreferrer">
-          <img src="/twitter.svg" alt="Twitter" className="h-6" />
+          <img src="/images/twitter.svg" alt="Twitter" className="h-6" />
         </a>
         <a href="https://www.instagram.com/dashbyte/" target="_blank" rel="noreferrer">
-          <img src="/instagram.svg" alt="Instagram" className="h-6" />
+          <img src="/images/instagram.svg" alt="Instagram" className="h-6" />
         </a>
       </div>
     </nav>
