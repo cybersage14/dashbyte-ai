22583b2e0b28cadf504046882808b77591e144c9
DarenGit28 <daren.shamoun28@gmail.com>
2023-06-29 16:25:45 -0700

    updated server so it connects to openAI

--- server/server.js
+++ server/server.js
@@ -1,22 +1,33 @@
 const express = require('express');
-const { OpenAIAPI } = require('openai');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
 
-const openai = new OpenAIAPI(process.env.OPENAI_API_KEY);
-
-app.post('/api/chat', async (req, res) => {
-    const messages = req.body.messages;
-    try {
-        const response = await openai.ChatCompletion.create({
-            model: 'gpt-3.5-turbo',
-            messages: messages
-        });
-        res.json({ message: response.data.choices[0].message.content });
-    } catch (error) {
-        res.status(500).json({ error: error.toString() });
-    }
+// Create an axios instance for OpenAI
+const openai = axios.create({
+  baseURL: 'https://api.openai.com/v1',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+  }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.post('/api/chat', async (req, res) => {
+  const messages = req.body.messages;
+
+  try {
+    // Send a chat completion request to the OpenAI API
+    const response = await openai.post('/chat/completions', {
+      model: 'gpt-3.5-turbo',
+      messages: messages
+    });
+
+    res.json({ message: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error('An error occurred while processing the chat:', error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.listen(5000, () => console.log('Server listening on port 5000'));
