7577f086fd230cf6a088df609560bcb0299e5a8d
DarenGit28 <daren.shamoun28@gmail.com>
2023-06-28 17:50:22 -0700

    creating header and home page

--- components/header.js
+++ components/header.js
@@ -1,15 +1,35 @@
-const Header = () => {
-    return (
-      <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-xl">Dashbyte AI</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
-  
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
+function Header() {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <Link href="/">
+        <a>
+          <img src="/logo.png" alt="Logo" className="h-8" />
+        </a>
+      </Link>
+      <ul className="flex space-x-4">
+        <li><Link href="/"><a className="text-white">Home</a></Link></li>
+        <li><Link href="/pc-builder"><a className="text-white">PC Builder</a></Link></li>
+        <li><Link href="/services"><a className="text-white">Services</a></Link></li>
+        <li><Link href="/about"><a className="text-white">About</a></Link></li>
+        <li><Link href="/contact"><a className="text-white">Contact</a></Link></li>
+      </ul>
+      <div className="flex space-x-4">
+        <a href="https://www.facebook.com/dashbyte/" target="_blank" rel="noreferrer">
+          <img src="/facebook.svg" alt="Facebook" className="h-6" />
+        </a>
+        <a href="https://twitter.com/dash_byte" target="_blank" rel="noreferrer">
+          <img src="/twitter.svg" alt="Twitter" className="h-6" />
+        </a>
+        <a href="https://www.instagram.com/dashbyte/" target="_blank" rel="noreferrer">
+          <img src="/instagram.svg" alt="Instagram" className="h-6" />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './layout';
+import Layout from '../components/layout';
 
 const Page = () => {
   return (
