56a04ba3a6cd5403cc0abcddbc2e9e3db1532295
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-02 19:48:28 -0700

    Added testing for the chatDisplay

--- components/chatDisplay.js
+++ components/chatDisplay.js
@@ -1,12 +1,17 @@
+import React from 'react';
+
 function ChatDisplay({ messages }) {
     return (
-      <div>
-        {messages.map((message, index) => (
-          <p key={index}>{message.role}: {message.content}</p>
-        ))}
-      </div>
+        <div className="flex flex-col space-y-4 p-3">
+            {messages.map((message, index) => (
+                <div key={index} className={`flex items-start ${message.role === 'user' ? 'justify-end' : 'justify-start'}`}>
+                    <div className={`rounded-lg px-4 py-2 ${message.role === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'}`}>
+                        {message.content}
+                    </div>
+                </div>
+            ))}
+        </div>
     );
-  }
-  
-  export default ChatDisplay;
-  
+}
+
+export default ChatDisplay;
