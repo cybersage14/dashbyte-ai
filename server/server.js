2fa2c5a69f59c85a6ea244383107bec0c7e4ada6
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-05 17:46:27 -0700

    added some logging

--- server/server.js
+++ server/server.js
@@ -11,13 +11,20 @@ app.use(cors());
 let _db;
 
 async function connectToDb() {
-  const client = new MongoClient(process.env.MONGO_URI, { useUnifiedTopology: true });
-  await client.connect();
-  _db = client.db('user_benchmarks');
-  console.log('Connected to MongoDB');
+  try {
+    const client = new MongoClient(process.env.MONGO_URI, { useUnifiedTopology: true });
+    await client.connect();
+    _db = client.db('user_benchmarks');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err);
+  }
 }
 
 function getDb() {
+  if (!_db) {
+    console.error('Database not initialized');
+  }
   console.log('Getting DB'); // Log a message every time getDb is called
   return _db;
 }
@@ -28,9 +35,10 @@ app.get('/api/parts/:partType', async (req, res) => {
     const db = getDb();
     console.log(`Fetching parts from collection: ${partType}_UserBenchmarks`);
     const parts = await db.collection(`${partType}_UserBenchmarks`).find().toArray();
+    console.log(`Fetched ${parts.length} parts from collection: ${partType}_UserBenchmarks`);
     res.json(parts);
   } catch (err) {
-    console.error(`Failed to fetch parts:`, err); // Log the entire error object
+    console.error(`Failed to fetch parts from collection: ${req.params.partType}_UserBenchmarks`, err); // Log the entire error object
     res.status(500).json({ error: 'Failed to fetch parts.' });
   }
 });
