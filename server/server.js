f2310910a0afd6c4b33b60fd32395eb7e7475a37
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-05 17:35:04 -0700

    working on database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
