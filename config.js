547e2a5aef656e4d1b85e35ebb647d72883607b2
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-08 00:18:28 -0700

    updated config.js

--- config.js
+++ config.js
@@ -3,5 +3,4 @@ require('dotenv').config({ path: '.env.local' });
 module.exports = {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'default',
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
-  OPENAI_API_URL: process.env.OPENAI_API_URL || 'https://api.openai.com',
 };
