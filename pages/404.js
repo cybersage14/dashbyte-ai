030bb4187261621de05c1501a42e16d5e69a76e9
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-07 17:45:04 -0700

    fixed link syntax error

--- pages/404.js
+++ pages/404.js
@@ -8,8 +8,8 @@ const Custom404 = () => {
       <div className="flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-6xl font-bold text-gray-800">404</h1>
         <p className="mt-4 text-xl text-gray-600">Sorry, we couldn't find the page you're looking for.</p>
-        <Link href="/">
-          <a className="mt-8 text-lg text-blue-500">Go back home</a>
+        <Link href="/" className="mt-8 text-lg text-blue-500">
+          Go back home
         </Link>
       </div>
     </Layout>
