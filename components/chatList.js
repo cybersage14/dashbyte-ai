62a1db47e3a785cb001ec5d08f3d59e6e268097a
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-09 01:49:33 -0700

    removd the system messages for now

--- components/chatList.js
+++ components/chatList.js
@@ -8,12 +8,14 @@ function ChatList({ messages }) {
 
   return (
     <div className="space-y-4 bg-gray-200 h-96 overflow-auto p-4">
-    {messages.length > 0 ? messages.filter(message => message.role !== 'system').map((message, index) => (
+    {messages.length > 0 ? messages.map((message, index) => (
+      !(message.role === 'system' && index === 0) && (
       <div key={index} className={`flex ${message.role === 'user' ? 'justify-end' : 'justify-start'} mx-2`}>
         <div className={`inline-block ${message.role === 'user' ? 'bg-user-blue text-white rounded-br-none' : 'bg-middle-gradient text-white rounded-bl-none'} rounded-md p-2`}>
           {message.content}
         </div>
       </div>
+      )
     )) : (
       <div className="max-w-xs mx-2 bg-white text-gray-800 rounded-md p-2">
         Start a conversation by typing a message below...
