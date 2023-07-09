0ac53a5a5c4652d85f3297bf281b349b83d23ef9
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-08 17:55:56 -0700

    CHAT IS WORKING

--- components/chat.js
+++ components/chat.js
@@ -4,62 +4,53 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addMessages, clearMessages } from '../redux/chatSlice';
 import axios from 'axios';
 import ChatPanel from './chatPanel';
-import { getAiMessage } from '../server/openai'; 
 
-//This is a React component that handles the chat interface. 
-//It uses the Redux state for managing chat messages and interacts with the OpenAI API
 function Chat() {
-  const chatState = useSelector(state => state.chat);  // Get the chat state from the Redux store
-  const dispatch = useDispatch();  // Get the dispatch function from the Redux store
-  const [input, setInput] = useState('');  // Store the user's input in the component's state
+  const chatState = useSelector(state => state.chat);
+  const dispatch = useDispatch();
+  const [input, setInput] = useState('');
 
-  // This function loads the chat history from the server when the component is first rendered
   useEffect(() => {
     const savedChat = localStorage.getItem('chat');
     dispatch(clearMessages());
     if (savedChat) {
       dispatch(addMessages(JSON.parse(savedChat)));
     } else {
-      getAiMessage([{ role: 'system', content: 'You are a helpful assistant, who specializes in helping user pick PC parts and build computers.' }])
-        .then(response => {
-          dispatch(addMessages(response.data.messages));
-        })
-        .catch(error => {
-          console.error('An error occurred while fetching the initial messages:', error);
-        });
+      const initialMessage = [{ role: 'system', content: 'Hello! I am your helpful assistant, ready to help you pick PC parts and build your dream computer. Please type your questions or requirements below.' }];
+      dispatch(addMessages(initialMessage));
     }
   }, []);
 
-  // This function saves the chat history to the browser's local storage when the chat history changes
   useEffect(() => {
     localStorage.setItem('chat', JSON.stringify(chatState.messages));
   }, [chatState.messages]);
 
-  // This function sends a message to the OpenAI API and adds the response to the Redux store
   const onSendMessage = () => {
     const newMessage = { role: 'user', content: input };
+    dispatch(addMessages([newMessage]));  // Add the user's message to the Redux store
+  
     const messages = [...chatState.messages, newMessage];
   
-    // Make a POST request to your own server
     axios.post('/api/chat', { messages })
       .then(response => {
-        const newMessagesFromServer = response.data.messages.map(message => ({ role: message.role, content: message.content }));
-        dispatch(addMessages(newMessagesFromServer));
+        const aiMessageContent = response.data.messages[response.data.messages.length - 1].content;
+        const aiMessage = { role: 'assistant', content: aiMessageContent };
+        dispatch(addMessages([aiMessage]));  // Add the AI's message to the Redux store
       })
       .catch(error => {
         console.error('An error occurred while sending the message:', error);
       });
+  
     setInput('');
   };
-  
-  // This function clears the chat history from the browser's local storage and Redux store
+
   const onClearChat = () => {
     localStorage.removeItem('chat');
     dispatch(clearMessages());
+    const initialMessage = [{ role: 'system', content: 'Hello! I am your helpful assistant, ready to help you pick PC parts and build your dream computer. Please type your questions or requirements below.' }];
+    dispatch(addMessages(initialMessage));
   };
-  
 
-  // This function handles the user pressing the Enter key
   return (
     <div className="flex flex-col h-full bg-gray-800 bg-opacity-50">
       <ChatPanel onSendMessage={onSendMessage} onClearChat={onClearChat} input={input} setInput={setInput} messages={chatState.messages} />
--- redux/chatSlice.js
+++ redux/chatSlice.js
@@ -10,10 +10,9 @@ const chatSlice = createSlice({
   reducers: {
     addMessages: (state, action) => {
       console.log('Dispatching addMessages action with payload:', action.payload);
-      state.messages.push(action.payload);
+      state.messages = [...state.messages, ...action.payload];
       console.log('Updated state:', state);
     },
-    
     clearMessages: (state) => {
       state.messages = [];
     },
