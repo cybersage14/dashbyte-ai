052a263ed882b7f6a0e713aa2a41c1ff894ff2c3
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-09 21:05:57 -0700

    making redux persist across reloads as well

--- pages/usePart.js
+++ pages/usePart.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; // Import the necessary hooks from react-redux
+import { selectPart } from '../redux/partSlice'; // Import the selectPart action
 
 // This custom hook fetches a list of parts from the server.
 const usePart = (partType) => {
   const [partList, setPartList] = useState([]);
-  const [selectedPart, setSelectedPart] = useState(null);
   const [error, setError] = useState(null);
+
+  // Get the selected part from the Redux store
+  const selectedPart = useSelector(state => state.selectedParts[partType]);
+
+  // Initialize the dispatch function
+  const dispatch = useDispatch();
 
   // Fetch the list of parts from the server.
   useEffect(() => {
@@ -27,7 +34,8 @@ const usePart = (partType) => {
 
   // This function is called when a part is selected.
   const onPartSelect = (part) => {
-    setSelectedPart(part);
+    // Update the selected part in the Redux store
+    dispatch(selectPart({ partType, part }));
   };
 
   return { partList, selectedPart, onPartSelect, error };
