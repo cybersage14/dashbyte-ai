3834be8975ff0cbe7bf8abfeb79068e55dc7b29a
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-18 17:26:53 -0700

    added comments for clarity

--- components/chat.js
+++ components/chat.js
@@ -5,56 +5,69 @@ import axios from 'axios';
 import ChatInput from './chatInput';
 import ChatList from './chatList';
 
+// chat component
 function Chat() {
-  const dispatch = useDispatch();
-  const messages = useSelector((state) => state.chat.messages);
-  const [input, setInput] = useState('');
+  const dispatch = useDispatch(); // dispatch function to dispatch actions to the store
+  const messages = useSelector((state) => state.chat.messages); // useSelector to extract data from the Redux store state
+  const [input, setInput] = useState(''); // input state to store the user's input
 
   useEffect(() => {
-    const savedChat = localStorage.getItem('chat');
-    dispatch(clearMessages());
-    if (savedChat) {
+    const savedChat = localStorage.getItem('chat'); // get the saved chat from local storage
+    dispatch(clearMessages()); // clear the messages in the store
+
+    // if there is a saved chat, add it to the store
+    if (savedChat) { 
       dispatch(addMessages(JSON.parse(savedChat)));
-    } else {
+    } 
+    // if there is no saved chat, add the initial message to the store
+    else { 
       const initialMessage = { 
         role: 'system', 
         content: "Hello! I am your helpful assistant, ready to help you pick PC parts and build your dream computer. Please type your questions or requirements below."
       };
       dispatch(addMessages([initialMessage]));      
     }
-  }, []);  
+  }, []);
 
+  // save the chat to local storage whenever the messages in the store change
   useEffect(() => {
-    localStorage.setItem('chat', JSON.stringify(messages));
+    localStorage.setItem('chat', JSON.stringify(messages)); // save the chat to local storage
   }, [messages]);
 
+  // send the user's message to the server and get the response
   const onSendMessage = () => {
-    const newMessage = { role: 'user', content: input };
-    dispatch(addMessages([newMessage]));  
+    const newMessage = { role: 'user', content: input }; // create a new message object
+    dispatch(addMessages([newMessage])); // add the new message to the store
 
-    const messagesWithNew = [...messages, newMessage];
+    const messagesWithNew = [...messages, newMessage]; // create a new array of messages with the new message
   
+    // send the messages to the server
     axios.post('/api/chat', { messages: messagesWithNew })
+      // if the request is successful, add the AI's response to the store
       .then(response => {
         const aiMessageContent = response.data.messages[response.data.messages.length - 1].content;
         const aiMessage = { role: 'assistant', content: aiMessageContent };
         dispatch(addMessages([aiMessage]));  
       })
+      // if the request is unsuccessful, log the error
       .catch(error => {
         console.error('An error occurred while sending the message:', error);
       });
 
-    setInput('');
+    setInput(''); // clear the input
   };
 
+  // clear the chat
   const onClearChat = () => {
-    localStorage.removeItem('chat');
-    dispatch(clearMessages());
+    localStorage.removeItem('chat'); // remove the chat from local storage
+    dispatch(clearMessages()); // clear the messages in the store
+    
+    // add the initial message to the store
     const initialMessage = { 
       role: 'system', 
       content: "Hello! I am your helpful assistant, ready to help you pick PC parts and build your dream computer. Please type your questions or requirements below."
     };
-    dispatch(addMessages([initialMessage]));    
+    dispatch(addMessages([initialMessage]));
   };    
 
   return (
